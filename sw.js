df1204295b19812a573de00a89f369b6e2eef4bf
Russell Hossain <143940722+Russell1269@users.noreply.github.com>
2026-06-24 14:28:59 +0600

    Comment out caching logic in sw.js
    Comment out the caching logic and event listeners in the service worker.

--- sw.js
+++ sw.js
@@ -1,65 +1,65 @@
-const CACHE_NAME = "my-site-cache-v1";
+// const CACHE_NAME = "my-site-cache-v1";
 
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./data.js",
-  "./about.html",
-  "./common.html",
-  "./deadlines.html",
-  "./exams.html",
-  "./offdays.html",
-];
+// const FILES_TO_CACHE = [
+//   "./",
+//   "./index.html",
+//   "./style.css",
+//   "./script.js",
+//   "./data.js",
+//   "./about.html",
+//   "./common.html",
+//   "./deadlines.html",
+//   "./exams.html",
+//   "./offdays.html",
+// ];
 
-// INSTALL
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    }),
-  );
-  self.skipWaiting();
-});
+// // INSTALL
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll(FILES_TO_CACHE);
+//     }),
+//   );
+//   self.skipWaiting();
+// });
 
-// ACTIVATE
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        }),
-      );
-    }),
-  );
-  self.clients.claim();
-});
+// // ACTIVATE
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(
+//     caches.keys().then((keys) => {
+//       return Promise.all(
+//         keys.map((key) => {
+//           if (key !== CACHE_NAME) {
+//             return caches.delete(key);
+//           }
+//         }),
+//       );
+//     }),
+//   );
+//   self.clients.claim();
+// });
 
-// FETCH (Cache First Strategy)
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      }
+// // FETCH (Cache First Strategy)
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       if (response) {
+//         return response;
+//       }
 
-      return fetch(event.request)
-        .then((networkResponse) => {
-          return networkResponse;
-        })
-        .catch(() => {
-          // If it's a page navigation, return index.html
-          if (event.request.mode === "navigate") {
-            return caches.match("./index.html");
-          }
+//       return fetch(event.request)
+//         .then((networkResponse) => {
+//           return networkResponse;
+//         })
+//         .catch(() => {
+//           // If it's a page navigation, return index.html
+//           if (event.request.mode === "navigate") {
+//             return caches.match("./index.html");
+//           }
 
-          // Otherwise return a basic empty response
-          return new Response("", { status: 404 });
-        });
-    })
-  );
-});
+//           // Otherwise return a basic empty response
+//           return new Response("", { status: 404 });
+//         });
+//     })
+//   );
+// });
